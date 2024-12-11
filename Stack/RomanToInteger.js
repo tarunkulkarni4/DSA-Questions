b064b13d303f2557c6d764af752c66707cf2d924
tarun <167800861+tarun-kulkarni-coder@users.noreply.github.com>
2024-12-11 22:37:23 +0530

    first commit

--- Stack/RomanToInteger.js
+++ Stack/RomanToInteger.js
@@ -1,6 +1,5 @@
 {
   /*
-
     Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
 Symbol       Value
--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -16,6 +16,35 @@
       return new int[]{};
     }
 }
+
+
+         OR
+
+         import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            // If the complement exists in the map, return the indices
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            
+            // Otherwise, store the number and its index
+            map.put(nums[i], i);
+        }
+        
+        return new int[] {}; // Return an empty array if no solution found
+    }
+}
+
+
     */
+
+              
 }
  
